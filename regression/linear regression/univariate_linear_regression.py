6eacf2a32a7b9d19a4e575d085608f19bbbbdf98
mohsinhaider <mohsinshaider@gmail.com>
2017-05-10 00:35:52 -0500

    Visualizaztion of training set and test set vs linear regression predictions

--- regression/linear regression/univariate_linear_regression.py
+++ regression/linear regression/univariate_linear_regression.py
@@ -12,7 +12,7 @@ response_matrix = dataset.iloc[:, [1]].values
 
 # Split the training and testing data sets
 from sklearn.cross_validation import train_test_split
-feature_train, feature_test, reponse_train, response_test =\
+feature_train, feature_test, response_train, response_test =\
     train_test_split(feature_matrix, response_matrix, \
                      test_size=1/3, random_state=0)
     
@@ -23,3 +23,19 @@ regressor.fit(feature_train, resp_train)
 
 ''' Predicing Test Set values using LinearRegression.predict()'''
 regressor.predict(feature_test)
+
+''' Visualize Training Set vs regressor.predict(feature_train) '''
+plt.scatter(feature_train, response_train, color="red")
+plt.plot(feature_train, regressor.predict(feature_train), color="blue")
+plt.xlabel("Years of Experience")
+plt.ylabel("Salary")
+plt.title("Salary vs Years of Experiences (Training Set)")
+plt.show()
+
+''' Visualize Test Set vs resp_test_prediction '''
+plt.scatter(feature_test, response_test, color="red")
+plt.plot(feature_test, resp_test_prediction, color="blue")
+plt.xlabel("Years of Experience")
+plt.ylabel("Salary")
+plt.title("Salary vs Years of Experience (Test Set)")
+plt.show()
